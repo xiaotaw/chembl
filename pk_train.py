d582aaccd6cbbd4b0e820c0cbf50f1b7fe373e91
xiaotaw_scw4750 <xiaotaw@qq.com>
2016-08-22 20:45:39 +0800

    speed up train_step by cutting off unnecessary computation

--- pk_train.py
+++ pk_train.py
@@ -15,44 +15,66 @@ import numpy
 import random
 import tensorflow as tf
 
+import pk_input
 import pk_model
 
-def train(dataset_dict, neg_dataset, target_list, out_filename, batch_size=128,
-          ckpt_dir = "/tmp/train", train_from = 0, max_step = 1000, 
-          pretrained_variables = False, keep_prob = 0.8,
-          start_learning_rate = 0.0, decay_step = 16000, decay_rate = 0.7):
 
-  """ train the model """  
+def train(target_list, train_from = 0):
+
+  # model parameters
+  batch_size = 128
+  ckpt_dir = "ckpt_files"
+  log_dir = "log_files"
+  max_step = 5000
+  keep_prob = 0.8
+  start_learning_rate = 0.05
+  decay_step = 5000
+  decay_rate = 0.7
+
+
+   
   # train log file
-  outfile = open(out_filename, 'w')
-  outfile.write("train starts at: %s\n" % datetime.datetime.now())
+  log_path = os.path.join(log_dir, "protein_kinase.log")
+  logfile = open(log_path, 'w')
+  logfile.write("train starts at: %s\n" % datetime.datetime.now())
 
-  with tf.Graph().as_default():
-    
-    input_placeholder = tf.placeholder(tf.float32, shape = (None, 2048))
-    label_placeholder = tf.placeholder(tf.float32, shape = (None, 2))
 
+  # get input dataset
+  train_dataset_dict = dict()
+  test_dataset_dict = dict()
+  for target in target_list:
+    train_dataset_dict[target] = pk_input.get_inputs_by_cpickle("data_files/pkl_files/" + target + "_train.pkl") 
+    test_dataset_dict[target] = pk_input.get_inputs_by_cpickle("data_files/pkl_files/" + target + "_test.pkl") 
+
+  neg_dataset = pk_input.get_inputs_by_cpickle("data_files/pkl_files/pubchem_neg_sample.pkl")
+
+
+
+  # train the model 
+  with tf.Graph().as_default(), tf.device("/gpu:1"):
+
+    # exponential decay learning rate
     global_step = tf.Variable(train_from, trainable=False)
-
     learning_rate = tf.train.exponential_decay(start_learning_rate, global_step, decay_step, decay_rate)
 
-    # build a Graph that computes the softmax predictions from the
-    # inference model.
+    # build the model
+    input_placeholder = tf.placeholder(tf.float32, shape = (None, 2048))
+    label_placeholder = tf.placeholder(tf.float32, shape = (None, 2))
+    # build the "Tree" with a mutual "Term" and several "Branches"
     base = pk_model.term(input_placeholder)
-
     softmax_dict = dict()
+    wd_loss_dict = dict()
+    x_entropy_dict = dict()
     loss_dict = dict()
     accuracy_dict = dict()
     train_op_dict = dict()
-
     for target in target_list:
       # compute softmax
       softmax_dict[target] = pk_model.branch(target, base)
       # compute loss.
-      term_wd_loss = tf.get_collection("term_wd_loss")
-      branch_wd_loss = tf.get_collection(target+"_wd_loss")
-      x_entropy = pk_model.x_entropy(softmax_dict[target], label_placeholder, target)
-      loss_dict[target] = tf.add_n(term_wd_loss + branch_wd_loss + [x_entropy])
+      wd_loss_dict[target] = tf.add_n(tf.get_collection("term_wd_loss") + tf.get_collection(target+"_wd_loss"))
+      x_entropy_dict[target] = pk_model.x_entropy(softmax_dict[target], label_placeholder, target)
+      loss_dict[target]  = tf.add(wd_loss_dict[target], x_entropy_dict[target])
       # compute accuracy
       accuracy_dict[target] = pk_model.accuracy(softmax_dict[target], label_placeholder, target)
       # train op
@@ -62,65 +84,73 @@ def train(dataset_dict, neg_dataset, target_list, out_filename, batch_size=128,
     saver = tf.train.Saver(tf.trainable_variables(), max_to_keep=None)
 
     # start running operations on the Graph.
-    sess = tf.Session()
+    config = tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)
+    sess = tf.Session(config=config)
     
     # initialize all variables at first.
     sess.run(tf.initialize_all_variables())
-    """
-    # restore from some train steps
-    if train_from > 0:
-      saver.restore(sess, ckpt_dir + '/model.ckpt-' + str(train_from))
-      sess.run(tf.initialize_variables([global_step], name="init_non_trainable_variables"))
 
-    if pretrained_variables:
-      # restore from pretrained variables
-      variables_to_restore = tf.get_collection("drug_1501")
-      saver_pre = tf.train.Saver(variables_to_restore)
-      saver_pre.restore(sess, pretrained_variables)
-    """
-
+    # define train batch
     perm = range(batch_size * 2)
+    compds_batch = numpy.zeros([batch_size * 2, 2048])
+    labels_batch = numpy.zeros([batch_size * 2, 2])
 
-    print("  step g_step   loss learn_rate    TP    FN    TN    FP    SEN    SPE    ACC    MCC sec/step target")
     # train with max step
+    print("  step g_step wdloss x_loss learn_rate    TP    FN    TN    FP    SEN    SPE    ACC    MCC t1-t0 t2-t1  target")
     for step in xrange(0, max_step):
-      neg_compds_batch, neg_labels_batch = neg_dataset.generate_batch(batch_size)
-
       for target in target_list:
-        start_time = time.time()
-        pos_compds_batch, pos_labels_batch = dataset_dict[target].generate_batch(batch_size)
+        t0 = time.time()
+        compds_batch[: batch_size], labels_batch[: batch_size] = neg_dataset.generate_batch(batch_size)
+        compds_batch[batch_size: ], labels_batch[batch_size: ] = train_dataset_dict[target].generate_batch(batch_size)
         random.shuffle(perm)
-        compds_batch = numpy.vstack([pos_compds_batch, neg_compds_batch])[perm]
-        labels_batch = numpy.vstack([pos_labels_batch, neg_labels_batch])[perm]
-        _, LV, LR, ACC, prediction, label_dense = sess.run(
-          [train_op_dict[target], 
-           loss_dict[target], 
-           learning_rate, 
-           accuracy_dict[target], 
-           tf.argmax(softmax_dict[target], 1), 
-           tf.argmax(labels_batch, 1)], 
-          feed_dict = {
-            input_placeholder: compds_batch,
-            label_placeholder: labels_batch,
-          }
-        )
-        duration = float(time.time() - start_time)
-      
+        compds_batch = compds_batch[perm]
+        labels_batch = labels_batch[perm]
+
+        _ = sess.run(train_op_dict[target], feed_dict={input_placeholder: compds_batch, label_placeholder: labels_batch})
+
+        t1 = float(time.time())
+
         # compute performance
         if step % 100 ==0 or (step + 1) == max_step:
-          TP,TN,FP,FN,SEN,SPE,MCC = pk_model.compute_performance(label_dense, prediction)
+          LV, XLV, LR, ACC, prediction, label_dense = sess.run(
+            [wd_loss_dict[target], 
+             x_entropy_dict[target],
+             learning_rate, 
+             accuracy_dict[target], 
+             tf.argmax(softmax_dict[target], 1), 
+             tf.argmax(labels_batch, 1)], 
+            feed_dict = {
+              input_placeholder: compds_batch,
+              label_placeholder: labels_batch,
+            }
+          )
+
+          TP, TN, FP, FN, SEN, SPE, MCC = pk_model.compute_performance(label_dense, prediction)
         
+          t2 = float(time.time())  
+
           # print to file and screen
-          format_str = "%6d %6d %6.3f %10.3f %5d %5d %5d %5d %6.3f %6.3f %6.3f %6.3f %8.3f %s"
-          outfile.write(format_str % (step, global_step.eval(sess), LV, LR, TP, FN, TN, FP, SEN, SPE, ACC, MCC, duration, target))
-          outfile.write('\n')
-          print(format_str % (step, global_step.eval(sess), LV, LR, TP, FN, TN, FP, SEN, SPE, ACC, MCC, duration, target))      
+          format_str = "%6d %6d %6.3f %6.3f %10.3f %5d %5d %5d %5d %6.3f %6.3f %6.3f %6.3f %5.3f %5.3f %s"
+          logfile.write(format_str % (step, global_step.eval(sess), LV, XLV, LR, TP, FN, TN, FP, SEN, SPE, ACC, MCC, t1-t0, t2-t1, target))
+          logfile.write('\n')
+          print(format_str % (step, global_step.eval(sess), LV, XLV, LR, TP, FN, TN, FP, SEN, SPE, ACC, MCC, t1-t0, t2-t1, target))      
 
         # save the model checkpoint periodically.
-        if step % 30 == 0 or (step + 1) == max_step:
+        if step % 1000 == 0 or (step + 1) == max_step:
           checkpoint_path = os.path.join(ckpt_dir, 'model.ckpt')
           saver.save(sess, checkpoint_path, global_step=global_step, write_meta_graph=False)
 
-  outfile.write("train ends at: %s\n" % datetime.datetime.now())
-  outfile.close()
+  logfile.write("train ends at: %s\n" % datetime.datetime.now())
+  logfile.close()
+
+
+
+if __name__ == "__main__":
+
+  target_list = ["cdk2", "egfr_erbB1", "gsk3b", "hgfr",
+                 "map_k_p38a", "tpk_lck", "tpk_src", "vegfr2"]
+
+  train(target_list)
+
+
 
--- train_single.py
+++ train_single.py
@@ -19,30 +19,38 @@ import pk_input
 import pk_model
 
 
-
-def train(target, batch_size=256, train_from = 0):
+def train(target, train_from = 0):
 
   """ train the model 
   """
   log_dir = "log_files"
   ckpt_dir = os.path.join("ckpt_files", target)
-  max_step = 20000
+  max_step = 3000
   keep_prob = 0.8,
   start_learning_rate = 0.05
   decay_step = 5000
   decay_rate = 0.7
+  batch_size = 256
+
   if not os.path.exists(ckpt_dir):
     os.makedirs(ckpt_dir)
 
   pos_dataset = pk_input.get_inputs_by_cpickle("data_files/pkl_files/" + target + "_train.pkl")
+  test_dataset = pk_input.get_inputs_by_cpickle("data_files/pkl_files/" + target + "_test.pkl")
   neg_dataset = pk_input.get_inputs_by_cpickle("data_files/pkl_files/pubchem_neg_sample.pkl")
+
+  compds_all = numpy.vstack([pos_dataset.compds, neg_dataset.compds])
+  labels_all = numpy.vstack([pos_dataset.labels, neg_dataset.labels])
+   
+  compds_test = test_dataset.compds
+  labels_test = test_dataset.labels
 
   # train log file
   log_path = os.path.join(log_dir, target + "_train_single.log")
   logfile = open(log_path, 'w')
   logfile.write("train starts at: %s\n" % datetime.datetime.now())
 
-  with tf.Graph().as_default():
+  with tf.Graph().as_default(), tf.device('/gpu:0'):
     
     input_placeholder = tf.placeholder(tf.float32, shape = (None, 2048))
     label_placeholder = tf.placeholder(tf.float32, shape = (None, 2))
@@ -59,20 +67,20 @@ def train(target, batch_size=256, train_from = 0):
     # compute softmax
     softmax = pk_model.branch(target, base)
     # compute loss.
-    term_wd_loss = tf.get_collection("term_wd_loss")
-    branch_wd_loss = tf.get_collection(target+"_wd_loss")
+    wd_loss = tf.add_n(tf.get_collection("term_wd_loss") + tf.get_collection(target+"_wd_loss"))
     x_entropy = pk_model.x_entropy(softmax, label_placeholder, target)
-    loss  = tf.add_n(term_wd_loss + branch_wd_loss + [x_entropy])
+    loss  = tf.add(wd_loss, x_entropy)
     # compute accuracy
     accuracy = pk_model.accuracy(softmax, label_placeholder, target)
     # train op
     train_op = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
 
     # create a saver.
-    saver = tf.train.Saver(tf.trainable_variables(), max_to_keep=None)
+    #saver = tf.train.Saver(tf.trainable_variables(), max_to_keep=None)
 
     # start running operations on the Graph.
-    sess = tf.Session()
+    config = tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)
+    sess = tf.Session(config=config)
     
     # initialize all variables at first.
     sess.run(tf.initialize_all_variables())
@@ -81,67 +89,102 @@ def train(target, batch_size=256, train_from = 0):
     compds_batch = numpy.zeros([batch_size, 2048])
     labels_batch = numpy.zeros([batch_size, 2])
 
-    print("  step g_step   loss learn_rate    TP    FN    TN    FP    SEN    SPE    ACC    MCC sec/step target")
+    print("  step g_step wdloss xloss learn_rate    TP    FN    TN    FP    SEN    SPE    ACC    MCC t1-t0 t2-t1  target")
+
     # train with max step
     for step in xrange(0, max_step):
 
       # prepare batch data
-      start_time = time.time()
+      t0 = time.time()
       compds_batch[:batch_size//2], labels_batch[:batch_size//2] = neg_dataset.generate_batch(batch_size//2)
       compds_batch[batch_size//2:], labels_batch[batch_size//2:] = pos_dataset.generate_batch(batch_size//2)
       random.shuffle(perm)
-      numpy.clip(compds_batch, 0, 1, out=compds_batch)
-      numpy.clip(labels_batch, 0, 1, out=labels_batch)
       compds_batch = compds_batch[perm]
       labels_batch = labels_batch[perm]
 
-      _, LV, LR, ACC, prediction, label_dense = sess.run(
-        [train_op, 
-         loss, 
-         learning_rate, 
-         accuracy, 
-         tf.argmax(softmax, 1), 
-         tf.argmax(labels_batch, 1)], 
+      _ = sess.run(
+        [train_op],
         feed_dict = {
           input_placeholder: compds_batch,
           label_placeholder: labels_batch,
         }
       )
-      duration = float(time.time() - start_time)
-      
+
+      t1 = float(time.time())
+
       # compute performance
       if step % 100 == 0 or (step + 1) == max_step:
-        TP,TN,FP,FN,SEN,SPE,MCC = pk_model.compute_performance(label_dense, prediction)
-        
-        # print to file and screen
-        format_str = "%6d %6d %6.3f %10.3f %5d %5d %5d %5d %6.3f %6.3f %6.3f %6.3f %8.3f %s"
-        logfile.write(format_str % (step, global_step.eval(sess), LV, LR, TP, FN, TN, FP, SEN, SPE, ACC, MCC, duration, target))
-        logfile.write('\n')
-        print(format_str % (step, global_step.eval(sess), LV, LR, TP, FN, TN, FP, SEN, SPE, ACC, MCC, duration, target))      
 
-
-      if step % 1000 == 0 or (step + 1) == max_step:
-                
-
-
-        LV, LR, ACC, prediction, label_dense = sess.run(
-          [loss, 
+        LV, XLV, LR, ACC, prediction, label_dense = sess.run(
+          [wd_loss, 
+           x_entropy,
            learning_rate, 
            accuracy, 
            tf.argmax(softmax, 1), 
-           tf.argmax(numpy.vstack([pos_dataset.labels, neg_dataset.labels]), 1)], 
+           tf.argmax(labels_batch, 1) 
+          ],
           feed_dict = {
-            input_placeholder: numpy.vstack([pos_dataset.compds, neg_dataset.compds]),
-            label_placeholder: numpy.vstack([pos_dataset.labels, neg_dataset.labels]),
+            input_placeholder: compds_batch,
+            label_placeholder: labels_batch,
           }
         )
+        TP,TN,FP,FN,SEN,SPE,MCC = pk_model.compute_performance(label_dense, prediction)
+
+        t2 = float(time.time())       
+
+        # print to file and screen
+        format_str = "%6d %6d %6.3f %6.3f %10.3f %5d %5d %5d %5d %6.3f %6.3f %6.3f %6.3f %5.3f %5.3f %s"
+        logfile.write(format_str % (step, global_step.eval(sess), LV, XLV, LR, TP, FN, TN, FP, SEN, SPE, ACC, MCC, t1-t0, t2-t1, target))
+        logfile.write('\n')
+        print(format_str % (step, global_step.eval(sess), LV, XLV, LR, TP, FN, TN, FP, SEN, SPE, ACC, MCC, t1-t0, t2-t1, target))      
+
+
+      # eval train and test dataset
+      if step % 1000 == 0 or (step + 1) == max_step:
+        t3 = float(time.time())
+        LV, XLV, LR, ACC, prediction, label_dense = sess.run(
+          [wd_loss, 
+           x_entropy,
+           learning_rate, 
+           accuracy, 
+           tf.argmax(softmax, 1), 
+           tf.argmax(labels_all, 1)], 
+          feed_dict = {
+            input_placeholder: compds_all,
+            label_placeholder: labels_all,
+          }
+        )
+        t4 = float(time.time())
         
         TP,TN,FP,FN,SEN,SPE,MCC = pk_model.compute_performance(label_dense, prediction)
         # print to file and screen
-        format_str = "%6d %6d %6.3f %10.3f %5d %5d %5d %5d %6.3f %6.3f %6.3f %6.3f %8.3f %s"
-        logfile.write(format_str % (step, global_step.eval(sess), LV, LR, TP, FN, TN, FP, SEN, SPE, ACC, MCC, duration, "test"))
+        format_str = "%6d %6d %6.3f %6.3f %10.3f %5d %5d %5d %5d %6.3f %6.3f %6.3f %6.3f %5.3f %5.3f %5.3f %s"
+        logfile.write(format_str % (step, global_step.eval(sess), LV, XLV, LR, TP, FN, TN, FP, SEN, SPE, ACC, MCC, t1-t0, t2-t1, t4-t3, "train"))
         logfile.write('\n')
-        print(format_str % (step, global_step.eval(sess), LV, LR, TP, FN, TN, FP, SEN, SPE, ACC, MCC, duration, "test"))      
+        print(format_str % (step, global_step.eval(sess), LV, XLV, LR, TP, FN, TN, FP, SEN, SPE, ACC, MCC, t1-t0, t2-t1, t4-t3, "train"))   
+
+        t5 = float(time.time())
+        LV, XLV, LR, ACC, prediction, label_dense = sess.run(
+          [wd_loss, 
+           x_entropy,
+           learning_rate, 
+           accuracy, 
+           tf.argmax(softmax, 1), 
+           tf.argmax(labels_test, 1)], 
+          feed_dict = {
+            input_placeholder: compds_test,
+            label_placeholder: labels_test,
+          }
+        )
+        t6 = float(time.time())
+        
+        TP,TN,FP,FN,SEN,SPE,MCC = pk_model.compute_performance(label_dense, prediction)
+        # print to file and screen
+        format_str = "%6d %6d %6.3f %6.3f %10.3f %5d %5d %5d %5d %6.3f %6.3f %6.3f %6.3f% 5.3f %5.3f %5.3f %s"
+        logfile.write(format_str % (step, global_step.eval(sess), LV, XLV, LR, TP, FN, TN, FP, SEN, SPE, ACC, MCC, t1-t0, t2-t1, t6-t5, "test"))
+        logfile.write('\n')
+        print(format_str % (step, global_step.eval(sess), LV, XLV, LR, TP, FN, TN, FP, SEN, SPE, ACC, MCC, t1-t0, t2-t1, t6-t5, "test"))      
+
 
       # save the model checkpoint periodically.
       #if step % 500 == 0 or (step + 1) == max_step:
@@ -152,5 +195,9 @@ def train(target, batch_size=256, train_from = 0):
   logfile.close()
 
 if __name__ == "__main__":
-  train("cdk2")
+
+  target_list = ["cdk2", "egfr_erbB1", "gsk3b", "hgfr",
+                 "map_k_p38a", "tpk_lck", "tpk_src", "vegfr2"]
+
+  train("egfr_erbB1")
 
