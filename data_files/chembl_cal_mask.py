eb52bc39948364a883d337fe214b7118ed4c4851
xiaotaw_scw4750 <xiaotaw@qq.com>
2016-12-27 16:19:09 +0800

    complete the comments to make code easy to understand

--- data_files/chembl_cal_mask.py
+++ data_files/chembl_cal_mask.py
@@ -50,7 +50,7 @@ for line in f:
 
 f.close()
 
-# read pns apfp
+# read (pubchem negative sample)pns apfp and counts the fps that appeared in pns compounds
 pns_id = []
 pns_apfp = {}
 pns_count = defaultdict(lambda : 0)
@@ -68,16 +68,21 @@ for line in f:
 
 f.close()
 
+
 # read top 79 targets' label
 clf_label_79 = np.genfromtxt(os.path.join(structure_dir, "chembl_top79.label"), usecols=[0, 2, 3], delimiter="\t", skip_header=1, dtype=str)
 
 
+################################################################################
 # generate sparse matrix for target features 
+
+# target compounds' chembl_id and clf label.
 target_clf_label = clf_label_79[clf_label_79[:, 0] == target]
 
-
+# target fps
 target_fps = [chembl_apfp[x] for x in target_clf_label[:, 1]]
 
+# count the fps that appeared in the compounds of the target
 target_count = defaultdict(lambda : 0)
 for fps_str in target_fps:
   for fp in fps_str[1:-1].split(","):
@@ -94,16 +99,22 @@ for k in target_count.keys():
 
 count_file.close()
   
-
+# pick out that fps that appeared for more than 10 times.
+# Here we assume that the more frequently a fp appeared, the more important it is.
 v = np.array([[k, target_count[k]] for k in target_count.keys()])
 m = v[:, 1] > 10
 target_apfp_picked = v[m][:, 0]
 
-
+# according to the apfp that picked out, define the columns in the feature sparse matrix
+# Note: a defaultdict is used. 
+# And the purpose is assign a default value(length of target_apfp_picked) for the apfps 
+# which is not included in target_apfp_picked. And this column(the last column) was finally 
+# not used at all.
 columns_dict = defaultdict(lambda : len(target_apfp_picked))
 for i, apfp in enumerate(target_apfp_picked):
   columns_dict[apfp] = i
 
+# define the function which can construct a feature sparse matrix according to the columns_dict
 def sparse_features(fps_list):
   data = []
   indices = []
@@ -120,20 +131,29 @@ def sparse_features(fps_list):
   a = sparse.csr_matrix((np.array(data), indices, indptr), shape=(len(fps_list), len(target_apfp_picked) + 1))
   return a
 
-# normally, abs(clf_label) > 0.5
-#target_pos_id = target_clf_label[target_clf_label[:, 2].astype(float) > 0.5][:, 1]
-
+# pick out target compounds with pos labels
+# normally, abs(clf_label) > 0.5(refer to chembl_preparation.py), 
+# so it also works when using the following line:
+# target_pos_id = target_clf_label[target_clf_label[:, 2].astype(float) > 0.5][:, 1]
 target_pos_id = target_clf_label[target_clf_label[:, 2].astype(float) > 0][:, 1]
 target_pos_fps = [chembl_apfp[x] for x in target_pos_id]
+
+# generate feature sparse matrix for target's pos compounds
 target_pos_features = sparse_features(target_pos_fps)[:, :-1].toarray()
 
+# generate feature sparse matrix for pns compounds
 target_pns_features = sparse_features([pns_apfp[k] for k in pns_id])[:, :-1]
 
-target_chembl_features = sparse_features([chembl_apfp[k] for k in chembl_id])[:, :-1]
+# generate feature sparse matrix for (chembl negative sample)cns compounds
+target_cns_features = sparse_features([chembl_apfp[k] for k in chembl_id])[:, :-1]
 
 
+################################################################################
 # generate mask for pns and cns
 
+# define a task function for sub process:
+# it can compare a part of negative sample(cns or pns) with pos samples,
+# and return the mask of those samples back to the main process.
 def sub_compare(sub_neg_id, sub_neg_features, conn):
   mask = {}
   log_str = []
@@ -145,6 +165,7 @@ def sub_compare(sub_neg_id, sub_neg_features, conn):
   conn.send((mask, log_str))
   conn.close()
 
+# the number of sub process for computation 
 n_jobs = 6
 
 
@@ -153,7 +174,6 @@ t1 = time.time()
 date1 = datetime.datetime.now()
 
 num_per_job = int(math.ceil(target_pns_features.shape[0] / float(n_jobs)))
-#num_per_job = 100
 thread_list = []
 conn_list = []
 for i in range(0, n_jobs):
@@ -176,7 +196,6 @@ t2 = time.time()
 
 target_pns_mask = defaultdict(lambda : False)
 
-
 log = open(log_dir + "/" + target + "_gen_pns_mask.log", "w")
 log.write("%s generate mask for pubchem neg sample, begins at %s\n" % (target, str(date1)))
 
@@ -193,27 +212,24 @@ mask_file = open(os.path.join(mask_dir, "%s_pns.mask" % target), "w")
 mask_file.writelines(["%s\t%s\n" % (x, target_pns_mask[x]) for x in pns_id])
 mask_file.close()
 
-
 print("generate mask for pns, duration: %.3f" % (t2 - t1))
-
 
 
 # for cns 
 t2 = time.time()
 date2 = datetime.datetime.now()
 
-num_per_job = int(math.ceil(target_chembl_features.shape[0] / float(n_jobs)))
-#num_per_job = 100
+num_per_job = int(math.ceil(target_cns_features.shape[0] / float(n_jobs)))
 thread_list = []
 conn_list = []
 for i in range(0, n_jobs):
   begin = i * num_per_job
   end = (i + 1) * num_per_job
-  if end > target_chembl_features.shape[0]:
-    end = target_chembl_features.shape[0]
+  if end > target_cns_features.shape[0]:
+    end = target_cns_features.shape[0]
   p_conn, c_conn = multiprocessing.Pipe()
   conn_list.append((p_conn, c_conn))
-  t = multiprocessing.Process(target=sub_compare, args=(chembl_id[begin: end], target_chembl_features[begin: end], c_conn))
+  t = multiprocessing.Process(target=sub_compare, args=(chembl_id[begin: end], target_cns_features[begin: end], c_conn))
   thread_list.append(t)
 
 for i in range(n_jobs):  
@@ -241,7 +257,6 @@ log.close()
 mask_file = open(os.path.join(mask_dir, "%s_cns.mask" % target), "w")
 mask_file.writelines(["%s\t%s\n" % (x, target_cns_mask[x]) for x in chembl_id])
 mask_file.close()
-
 
 print("generate mask for cns, duration: %.3f" % (t3 - t2))
 
